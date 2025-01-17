5b94838bc143147402a74a996e722ec0374886ff
Kosieun <sieun1156@naver.com>
2025-01-17 18:02:20 +0900

    refactor : AppCompatDelegate 사용하여 다국어 변환

--- app/src/main/java/com/example/translation/MainActivity.kt
+++ app/src/main/java/com/example/translation/MainActivity.kt
@@ -12,6 +12,8 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.example.translation.databinding.ActivityMainBinding
 import java.util.Locale
 
@@ -19,55 +21,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var deviceLanguage : String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val pref = getSharedPreferences("lan", 0)
-        val edit = pref.edit()
-
-        val storedLanguage = pref.getString("language","")
-
-        if(storedLanguage == ""){
-            deviceLanguage = Locale.getDefault().language
-        }else{
-            storedLanguage?.let { Locale(it) }?.let { Locale.setDefault(it) }
-        }
-
-        Log.i("@@@@", "deviceLanguage: $deviceLanguage")
 
         binding.mcvTranslationKoreanButton.setOnClickListener {
-            if (deviceLanguage == "ko") {
-                setLocale("en")
-                //Pref 에 저장
-
-                edit.putString("language", "en")
-                edit.apply()
-            } else {
-                setLocale("ko")
-                edit.putString("language", "ko")
-                edit.apply()
-
-            }
+            handleLanguageChange()
         }
     }
 
-    private fun setLocale(languageCode: String) {
-        val locale = Locale(languageCode)
-        Locale.setDefault(locale)
-
-        val configuration = resources.configuration
-        configuration.setLocale(locale)
-        configuration.setLayoutDirection(locale)
-        createConfigurationContext(configuration)
-
-        finish()
-        startActivity(packageManager.getLaunchIntentForPackage(packageName)?.apply {
-            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-        })
+    private fun handleLanguageChange() {
+        val currentLocale = AppCompatDelegate.getApplicationLocales()
+        val newLocale = if (currentLocale.toLanguageTags().contains("ko")) {
+            LocaleListCompat.forLanguageTags("en")
+        } else {
+            LocaleListCompat.forLanguageTags("ko")
+        }
+        AppCompatDelegate.setApplicationLocales(newLocale)
     }
 }
